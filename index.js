4e8fbd17ccafdd5536de551106c99ae8e74f2b7d
unknown <sarvarbekakramjonov51@gmail.com>
2026-02-26 21:10:57 +0500

    Add routes and update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { create } from "express-handlebars";
+import AuthRoutes from "./routes/auth.js";
+import ProductsRoutes from "./routes/products.js";
 
 const app = express();
 
@@ -12,25 +14,8 @@ app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "./views");
 
-app.get("/", (req, res) => {
-	res.render("index");
-});
-
-app.get("/add", (req, res) => {
-	res.render("add");
-});
-
-app.get("/products", (req, res) => {
-	res.render("products");
-});
-
-app.get("/login", (req, res) => {
-	res.render("login");
-});
-
-app.get("/register", (req, res) => {
-	res.render("register");
-});
+app.use(AuthRoutes);
+app.use(ProductsRoutes);
 
 const PORT = process.env.PORT || 4100;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
---
+++ routes/products.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (req, res) => {
+	res.render("index");
+});
+
+router.get("/add", (req, res) => {
+	res.render("add");
+});
+
+router.get("/products", (req, res) => {
+	res.render("products");
+});
+
+export default router;
