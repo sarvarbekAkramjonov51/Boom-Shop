b519e8a538dd3632a4119ddd897321e2fa03a2c3
unknown <sarvarbekakramjonov51@gmail.com>
2026-02-27 18:48:16 +0500

    Update auth flow and auth pages

--- routes/auth.js
+++ routes/auth.js
@@ -4,16 +4,26 @@ const router = Router();
 
 router.get("/login", (req, res) => {
 	res.render("login", {
-        title : "Login | Akramjonov",
-        isLogin : true
-    });
+		title: "Login | Akramjonov",
+		isLogin: true,
+	});
 });
 
 router.get("/register", (req, res) => {
 	res.render("register", {
-        title : "Register | Akramjonov",
-        isRegister : true
-    });
+		title: "Register | Akramjonov",
+		isRegister: true,
+	});
+});
+
+router.post("/login", (req, res) => {
+	console.log(req.body);
+	res.redirect("/");
+});
+
+router.post("/register", (req, res) => {
+	console.log(req.body);
+	res.redirect("/");
 });
 
 export default router;
