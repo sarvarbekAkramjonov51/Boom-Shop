83f7d37bc9b52441e8cfe29f3ba149f9416c8a7b
unknown <sarvarbekakramjonov51@gmail.com>
2026-03-01 09:39:43 +0500

    Update dependencies and footer partial

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { create } from "express-handlebars";
+import mongoose from "mongoose";
 import AuthRoutes from "./routes/auth.js";
 import ProductsRoutes from "./routes/products.js";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
@@ -13,12 +16,17 @@ const hbs = create({
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "./views");
+app.use(express.json());
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(AuthRoutes);
 app.use(ProductsRoutes);
+
+mongoose.set("strictQuery", false);
+await mongoose.connect(process.env.MONGO_URL);
+console.log("Connected to DB ✅");
 
 const PORT = process.env.PORT || 4100;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
